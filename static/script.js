613b14602cf1b6b73749cc894ecbf8d25f5bb0d6
Nagib Alexandre <80482671+NagibAlexandre@users.noreply.github.com>
2022-12-07 19:04:28 -0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,16 @@
+const menuMobile = document.querySelector(".menu-mobile")
+const body = document.querySelector("body")
+
+menuMobile.addEventListener("click",moveMenuLateral)
+
+function moveMenuLateral(){
+
+    if(menuMobile.classList.contains("bi bi-list")){
+        menuMobile.classList.replace("bi-list", "bi-x");
+    }else{
+        menuMobile.classList.replace("bi-x", "bi-list");
+    }
+
+    body.classList.toggle("menu-nav-active");
+
+}
